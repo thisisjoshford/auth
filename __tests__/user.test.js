902e0a3cbd9d38d114885105b3b597816875849d
Josh Ford <thisisjoshford@gmail.com>
2020-03-30 15:33:45 -0700

    creates a JWT test written and failing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -10,7 +10,16 @@ describe('User Model', () => {
 
     expect(user.toJSON().passwordHash).toEqual(expect.any(String));
     expect(user.toJSON().password).toBeUndefined();
-
   });
+
+  it('creates a JWT', () => {
+    const user = new User({
+      username: 'joshford',
+      password: 'superSecurePassword'
+    });
+
+    const token = user.authToken();
+    expect(token).not.toBeUndefined();
+  })
 });
 
