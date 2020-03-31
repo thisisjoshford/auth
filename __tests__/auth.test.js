b7098043dd117eaee7052f65c1e5ddd5d5045050
Josh Ford <thisisjoshford@gmail.com>
2020-03-30 22:52:00 -0700

    signs up a user test written and failing

---
+++ __tests__/auth.test.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('auth routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'j0shf0rd', password: 'sup3rs3cr3t' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'j0shf0rd',
+          __v: 0
+        });
+      });
+  });
+
+});
